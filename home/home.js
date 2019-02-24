308b6ba216565a9756b641969bc00118878bf118
Zaid Naikwadi <naikwadizaid@gmail.com>
2019-02-24 19:30:11 +0530

    Add new project done

--- home/home.js
+++ home/home.js
@@ -9,6 +9,8 @@
   };
   firebase.initializeApp(config);
 
+  var user = null;
+
     signoutBtn = document.getElementById("signout");
 
     signoutBtn.addEventListener('click',e => {
@@ -46,6 +48,7 @@
              // alert("writing");
              // writeUserData(firebaseUser.uid,firebaseUser.username,firebaseUser.email);
              var userId = firebase.auth().currentUser.uid;
+             user = firebaseUser;
 
              //PASS ABOVE USERID TO A FUNCTION AND INSERT OR WRITE WHILE CODING
              //DON'T WRITE EVERYTHING HERE
@@ -68,5 +71,70 @@
               console.log("not logged in");
           }
       });
+
+
+
+
+
+
+
+      /* For Adding new project
+      /* Zaid Naikwadi 24/02/19
+      /* 
+                                */
+
+      var title = document.getElementById("projectTitle");
+      var description = document.getElementById("projectDescription");
+      var submitBtn = document.getElementById("submit");
+      
+
+      
+
+      submitBtn.addEventListener("click", e =>{
+          var tags = document.getElementsByClassName("tag");
+          writeAddProjectData(title.value,description.value,tags);
+      });
+
+      function writeAddProjectData(title,description,tags){
+        
+          if(user){
+            var userId = firebase.auth().currentUser.uid;
+            console.log("Hey your user id: "+userId);
+
+            projectKey = firebase.database().ref('Projects/').push({
+              title: title,
+              description: description,
+              userId : userId
+            }).key;
+
+            /*for(var i=0;i<tags.length;i++){
+              console.log("Your tags: "+tags[i].innerHTML);
+              firebase.database().ref('Projects/').child(projectKey).child('tags/').push({
+                tag : tags[i].innerHTML
+              });
+              console.log("Key: "+projectKey);
+            }*/
+
+            var Alltags = {};
+
+            for(var i=0;i<tags.length;i++){
+              console.log("Your tags: "+tags[i].innerHTML);
+              Alltags["tags"+i] = {"tag": tags[i].innerHTML};
+            }
+
+            firebase.database().ref('Projects/'+projectKey+"/tags/").set(Alltags).then(function(){
+              alert("Project successfully added");
+              window.location = "home.html";
+            });
+
+          }
+          else{
+            console.log("How can you add project when you are not logged in");
+          }
+      }
+
+
+
+
 
 }());
