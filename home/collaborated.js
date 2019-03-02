8c799bf281f70703e1ede604c13186622c22e800
aakole515 <aakole515@gmail.com>
2019-03-02 16:20:47 +0530

    not done..........

---
+++ home/collaborated.js
@@ -0,0 +1,83 @@
+function main()
+{
+    getCollaboratedProjects();
+
+    function getCollaboratedProjects()
+    {
+        var ref = firebase.database().ref('collaborate');
+                                var pid,uid;
+                                 ref.on('value',function(snapshot){
+                                     console.log("data from collaborte : "+snapshot.val());
+                                      pid = snapshot.val().projectid;
+                                      uid=snapshot.val().userid;  
+                                      console.log(uid);
+                                      console.log(pid);
+                                    // if(snapshot.val()){
+                                    //  var keys = Object.keys(temp);
+                                    //  console.log(keys);
+                                    //    }
+                                    });//comment it later on
+
+                                    var ref=firebase.database().ref('Projects/'+uid+'/');
+                                    ref.on('value',function(snapshot){
+                                        console.log("data from project : "+snapshot.val());
+                                        var temp=snapshot.val();
+                                       // var keys = Object.keys(temp);
+                                         console.log(temp);
+                                        
+         });
+                                   
+                                //      for(var i=0;i<keys.length;i++)
+                                //      {
+                                //        var k = keys[i];
+                                //        var title = temp[k].title;
+                                //        var description = temp[k].description;
+                                //        var tags ="";
+                                //        console.log("title : " +title);
+                                //        console.log("Description : " +description);
+                                //        //For tags fetching from database by kamlesh silag on 25/02/2019
+                                //        var projectKey = keys[i];
+                                //        console.log("key abhi : "+projectKey);
+                                //        var ref2 =firebase.database().ref('Projects/'+userId+'/'+projectKey+"/tags/");
+                                //        ref2.on('value',function(snapshot2){
+                                //          console.log("data2 : "+snapshot2.val());
+                                         
+                                //          var temp2 = snapshot2.val();
+                                //          var keys2 = Object.keys(temp2);
+                                //          console.log(keys2);
+                         
+                                //          for(var j= 0;j<keys2.length;j++)
+                                //          {
+                                //            var k2 = keys2[j];
+                                //            if(j==0)
+                                //              //For first time no comma
+                                //              tags =tags+temp2[k2].tag+"";
+                                //            else  
+                                //              tags =tags+" , "+temp2[k2].tag;
+                                //          }
+                         
+                                //        });
+                         
+                                //        $(".rowmyprojects").append("<div class='col-lg-15 col-xs-12'>"+
+                                //        "<!-- small box -->"+
+                                //        "<div class='small-box bg-aqua'>"+
+                                //          "<div class='inner'>"+
+                                //            "<h3>"+title+"</h3>"+
+                                //            "<p><b>Description : </b>"+description+"</p>"+
+                                //            "<p><b>Tags : </b>"+tags+"</p>"+
+                                //          "</div>"+
+                                //          "<div class='icon'>"+
+                                //            "<i class='ion ion-bag'></i>"+
+                                //          "</div>"+
+                                //          "<a href='#' class='small-box-footer'>More info <i class='fa fa-arrow-circle-right'></i></a>"+
+                                //        "</div>"+
+                                //      "</div>")
+                                //      }
+                                //     }
+                                //     else{
+                                //       console.log("No my projects");
+                                //     }
+
+                                //  });
+    }
+}
